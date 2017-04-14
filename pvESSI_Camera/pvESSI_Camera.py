aa896a6ab0edf4bd88a4b00f4171668f71ec9fc3
Sumeet Kumar Sinha <sumeet.kumar507@gmail.com>
2017-04-14 00:25:09 -0700

    Updating pvESSI_camera

--- pvESSI_Camera/pvESSI_Camera.py
+++ pvESSI_Camera/pvESSI_Camera.py
@@ -13,21 +13,53 @@
 ####################################################################################### 
 
 from paraview.simple import *
-LoadPlugin("libpvESSI.so",ns=globals())
+LoadPlugin("libpvESSI.so",ns=globals());
 from paraview.simple import *
 
 def ResetSession():
-    pxm = servermanager.ProxyManager()
-    pxm.UnRegisterProxies()
-    del pxm
-    Disconnect()
-    Connect()
+    pxm = servermanager.ProxyManager();
+    pxm.UnRegisterProxies();
+    del pxm;
+    Disconnect();
+    Connect();
 
-ResetSession()
-pvESSI(FileName="ShearBox_Sequential.feioutput")
-Show()
-Render()
-viewLayout = GetLayout()
+ResetSession();
+ESSI_Output = pvESSI(FileName='/home/sumeet/sumeet.kumar507@gmail.com/git/pvESSI/Examples/ShearBox_Sequential.feioutput');
+
+# get animation scene
+animationScene1 = GetAnimationScene();
+
+# update animation scene based on data timesteps
+animationScene1.UpdateAnimationUsingDataTimeSteps();
+
+# get active view
+renderView1 = GetActiveViewOrCreate('RenderView');
+# uncomment following to set a specific view size
+# renderView1.ViewSize = [2405, 1879]
+
+# reset view to fit data
+renderView1.ResetCamera();
+
+# create a new 'Warp By Vector'
+warpByVector1 = WarpByVector(Input=ESSI_Output);
+warpByVector1.Vectors = ['POINTS', 'Generalized_Displacements'];
+warpByVector1.ScaleFactor = 50.37827622736076;
+
+# show data in view
+warpByVector1Display = Show(warpByVector1, renderView1)
+
+# set scalar coloring
+ColorBy(warpByVector1Display, ('POINTS', 'Generalized_Displacements'));
+
+# rescale color and/or opacity maps used to include current data range
+warpByVector1Display.RescaleTransferFunctionToDataRange(True, False);
+
+# show color bar/color legend
+warpByVector1Display.SetScalarBarVisibility(renderView1, True);
+
+#Show()
+#Render()
+#viewLayout = GetLayout()
 # save screenshot
-SaveScreenshot('filepath/filename-%s.png' % time,layout=viewLayout, magnification=3, quality=100)
+SaveScreenshot('/home/sumeet/sumeet.kumar507@gmail.com/git/REAL-ESSI_Utility_Tools/pvESSI_Camera/filename.png')
 
